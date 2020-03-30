5cdaa7d8e111787cd0a6784880a1b3cd5bed6af7
Alexandra <sunny.belyavskaya@gmail.com>
2020-03-30 23:49:13 +0300

    backgrounds

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import s from "./Main.module.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faReact} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
 function Main() {
-  return (
-    <div className={s.main}>
-      <div className={s.conteiner}>
-        <div className={s.greeting}>
-          {/*<FontAwesomeIcon icon={faReact}/>*/}
-          {/*<FontAwesomeIcon icon={faEnvelope}/>*/}
-          <span>Привет!</span>
-          <span>Меня зовут Белявская Александра!</span>
-          <span>Blablabla</span>
+    return (
+        <div className={s.main}>
+            <div className={s.conteiner}>
+                <div className={s.greeting}>
+                    {/*<FontAwesomeIcon icon={faReact}/>*/}
+                    {/*<FontAwesomeIcon icon={faEnvelope}/>*/}
+                    <span>Hello!</span>
+                    <span>I'm <span>Belyavskaya Alexandra</span></span>
+                    <h1>A front-end developer</h1>
+                </div>
+                <div className={s.photo}>
+                    <img src="" alt=""/>
+                </div>
+            </div>
         </div>
-        <div className={s.photo}>
-          <img src="" alt=""/>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Main;
