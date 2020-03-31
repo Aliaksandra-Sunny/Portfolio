7ad59cbf2a7a5dc149de8c86f4bb26c712bddfc2
Alexandra <sunny.belyavskaya@gmail.com>
2020-03-31 23:16:53 +0300

    scrolls were fixed, button component was created

--- src/components/Slogan/Slogan.jsx
+++ src/components/Slogan/Slogan.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import s from "./Slogan.module.css"
 import Heading from "../Elements/Heading/Heading";
+import Button from "../Elements/Button/Button";
 
 function Slogan() {
   return (
     <div className={s.slogan}>
       <div className={s.container}>
           <Heading name={"I am available for Freelancer"}/>
-        <button>Нанять меня</button>
+          <Button name={"Hire me"}/>
       </div>
     </div>
   );
