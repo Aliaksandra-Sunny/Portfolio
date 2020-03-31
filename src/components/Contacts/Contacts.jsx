b70345f01664ad1a06c6dce6f67960f241ee0f5f
Alexandra <sunny.belyavskaya@gmail.com>
2020-04-01 01:26:00 +0300

    form style

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -3,22 +3,27 @@ import s from "./Contacts.module.css"
 import Heading from "../Elements/Heading/Heading";
 
 function Contacts() {
-  return (
-    <div className={s.contacts}>
-      <div className={s.container}>
-        <Heading name={"Contact"}/>
-        <div className={s.form}>
-          <form>
-            <input/>
-            <input/>
-            <textarea>
-          </textarea>
-          </form>
-          <input type="submit"/>
+    return (
+        <div className={s.contacts}>
+            <div className={s.container}>
+                <Heading name={"Contact"}/>
+                <div className={s.form}>
+                    <form>
+                        <div className={s.formContainer}>
+                            <input placeholder="Name" className={s.formItem}/>
+                        </div>
+                        <div className={s.formContainer}>
+                            <input placeholder="E-mail" className={s.formItem}/>
+                        </div>
+                        <div className={s.formContainer}>
+                            <textarea placeholder="Your message" className={s.formItem}/>
+                        </div>
+                    </form>
+                    <input type="submit"/>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Contacts;
